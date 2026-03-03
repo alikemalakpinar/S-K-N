c0c4c72edc12c14aa773548edd160335200e7de4
AICO Software & Design <alikemal.gs@hotmail.com>
2026-03-03 15:35:16 +0300

    Initial Commit

---
+++ SÜKÛN/SU_KU_NApp.swift
@@ -0,0 +1,17 @@
+//
+//  SU_KU_NApp.swift
+//  SÜKÛN
+//
+//  Created by Ali kemal Akpinar on 3.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SU_KU_NApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
